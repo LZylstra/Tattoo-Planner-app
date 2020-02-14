c29241a6a37f46e644edef97c919df6ce2a5ea6b
LZylstra <lindseyzylstra@gmail.com>
2020-02-13 20:56:25 -0800

    added all elements of landing page, minimal styling

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Icon.css";
+
+function Icon() {
+  return (
+    <section className="icon">
+      <div className="box">
+        <img src="https://via.placeholder.com/100" alt="icon" />
+        <h3>Icon Name</h3>
+      </div>
+    </section>
+  );
+}
+
+export default Icon;
--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./LandingPage.css";
+import Icon from "../../components/Icon/Icon";
 
 function LandingPage() {
-  return <div>Landing Page</div>;
+  return (
+    <div className="landing_page">
+      <p className="app_description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et
+        vehicula justo, id ullamcorper augue. Nam ornare sit amet tellus ac
+        mollis. Phasellus id sapien ipsum.
+      </p>
+      <img
+        id="demo_pic"
+        src="https://via.placeholder.com/800x500"
+        alt="demo of app"
+      />
+      <Icon />
+      <Icon />
+      <Icon />
+
+      <div className="demo_section">
+        <button>Try it Out</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
