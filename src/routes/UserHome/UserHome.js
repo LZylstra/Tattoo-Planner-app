a864ba1558255934376e4c834f8d4ca651927271
LZylstra <lindseyzylstra@gmail.com>
2020-02-15 12:34:08 -0800

    added placeholder for future calendar

--- src/routes/UserHome/UserHome.js
+++ src/routes/UserHome/UserHome.js
@@ -3,22 +3,30 @@ import Icon from "../../components/Icon/Icon";
 import financialIcon from "../../img/financial-icon.png";
 import clientIcon from "../../img/client-icon.png";
 import tattooIcon from "../../img/tattoo-icon.png";
+import calendarPlaceholder from "../../img/calendar-placeholder.png";
 import { Link } from "react-router-dom";
 import "./UserHome.css";
 
 function UserHome() {
   return (
     <div className="home">
-      User Home Page
-      <Link to="/client-list">
-        <Icon title="Clients" picture={clientIcon} />
-      </Link>
-      <Link to="/ledger">
-        <Icon title="Ledger" picture={financialIcon} />
-      </Link>
-      <Link to="/tattoo-list">
-        <Icon title="Tattoos" picture={tattooIcon} />
-      </Link>
+      <div className="calendar-section">
+        <img
+          src={calendarPlaceholder}
+          alt="temporary placeholder for calendar"
+        />
+      </div>
+      <div className="icon-row">
+        <Link to="/client-list">
+          <Icon title="Clients" picture={clientIcon} />
+        </Link>
+        <Link to="/ledger">
+          <Icon title="Ledger" picture={financialIcon} />
+        </Link>
+        <Link to="/tattoo-list">
+          <Icon title="Tattoos" picture={tattooIcon} />
+        </Link>
+      </div>
     </div>
   );
 }
