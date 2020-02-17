24b8b199cd47dc3c5c823ba93f6ed9cf70d48a89
LZylstra <lindseyzylstra@gmail.com>
2020-02-17 15:15:12 -0800

    added calendar

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "https://floating-forest-01615.herokuapp.com//api",
+  API_ENDPOINT: "https://floating-forest-01615.herokuapp.com/api",
   TOKEN_KEY: "tattoo-auth-token"
 };
